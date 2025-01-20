6f14fbf9a880b0a990d7490adf234a285e3da895
Dharanidharan S <dharanidharan.s@technoforte.co.in>
2025-01-20 16:55:36 +0530

    preprod install scripts

--- deployment/v3/mosip/idrepo/install.sh
+++ deployment/v3/mosip/idrepo/install.sh
@@ -22,19 +22,19 @@ function installing_idrepo() {
   ./copy_cm.sh
 
   echo Running salt generator job
-  helm -n $NS install idrepo-saltgen  mosip/idrepo-saltgen --set image.repository=niradocker/id-repository-salt-generator --set image.tag=niradev-1.2.0.1-N1 --version $CHART_VERSION --wait --wait-for-jobs
+  helm -n $NS install idrepo-saltgen  mosip/idrepo-saltgen --set image.repository=mosipid/id-repository-salt-generator --set image.tag=1.2.0.1 --version $CHART_VERSION --wait --wait-for-jobs
 
   echo Running credential
-  helm -n $NS install credential mosip/credential --set image.repository=niraqa/credential-service --set image.tag=tf_nira_qa --version $CHART_VERSION
+  helm -n $NS install credential mosip/credential --set image.repository=niraqa/credential-service --set image.tag=tf_nira_qa_new --version $CHART_VERSION
 
   echo Running credential request service
-  helm -n $NS install credentialrequest mosip/credentialrequest --set image.repository=niraqa/credential-request-generator --set image.tag=tf_nira_qa --version $CHART_VERSION
+  helm -n $NS install credentialrequest mosip/credentialrequest --set image.repository=niraqa/credential-request-generator --set image.tag=tf_nira_qa_new --version $CHART_VERSION
 
   echo Running identity service
-  helm -n $NS install identity mosip/identity --set image.repository=niraqa/id-repository-identity-service --set image.tag=tf_nira_qa --version $CHART_VERSION
+  helm -n $NS install identity mosip/identity --set image.repository=niraqa/id-repository-identity-service --set image.tag=tf_nira_qa_new --version $CHART_VERSION
 
   echo Running vid service
-  helm -n $NS install vid mosip/vid --set image.repository=niraqa/id-repository-vid-service --set image.tag=tf_nira_qa--version $CHART_VERSION
+  helm -n $NS install vid mosip/vid --set image.repository=niraqa/id-repository-vid-service --set image.tag=tf_nira_qa_new--version $CHART_VERSION
 
   kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
   echo Installed idrepo services
