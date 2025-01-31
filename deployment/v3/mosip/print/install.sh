68ae226856eee1826c92765b1fbb6bf82f0ce6f9
sjkarthik <58928645+sjkarthik@users.noreply.github.com>
2025-01-31 23:49:16 +0530

    Update install.sh

--- deployment/v3/mosip/print/install.sh
+++ deployment/v3/mosip/print/install.sh
@@ -23,7 +23,7 @@ function installing_print() {
   ./copy_cm.sh
 
   echo Installing print service
-  helm -n $NS install print-service mosip/print-service --set image.repository=niraqa/print --set image.tag=tf_nira_qa_new --wait --version $CHART_VERSION
+  helm -n $NS install print-service mosip/print-service --set image.repository=niraqa/print --set image.tag=pre-production --wait --version $CHART_VERSION
   return 0
 }
 
