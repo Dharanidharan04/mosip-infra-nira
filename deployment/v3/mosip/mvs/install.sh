e55c87b7f6be63d0fbde7495f0d419e6bca311c3
mdminhaj88 <md.minhaj88@gmail.com>
2025-02-01 10:53:50 +0530

    added mvs

---
+++ deployment/v3/mosip/mvs/install.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+# Installs the mvs module
+# Make sure you have updated ui_values.yaml
+## Usage: ./install.sh [kubeconfig]
+
+if [ $# -ge 1 ] ; then
+  export KUBECONFIG=$1
+fi
+
+NS=mvs
+CHART_VERSION=12.0.1
+
+echo Create $NS namespace
+kubectl create ns $NS
+
+helm repo add mvs-service https://tf-nira.github.io/mosip-helm/
+helm repo add mvs-ui https://tf-nira.github.io/mosip-helm/
+
+function installing_mvs() {
+  echo Istio label
+  kubectl label ns $NS istio-injection=enabled --overwrite
+  helm repo update
+
+  echo Copy configmaps
+  sed -i 's/\r$//' copy_cm.sh
+  ./copy_cm.sh
+
+  echo login to docker
+  sed -i 's/\r$//' dockerlogin.sh
+  ./dockerlogin.sh
+
+
+  API_HOST=$(kubectl get cm global -o jsonpath={.data.mosip-api-internal-host})
+  mvs_HOST=$NS.$(kubectl get cm global -o jsonpath={.data.installation-domain})
+
+  echo Installing mvs-Proxy into Masterdata and Keymanager.
+  kubectl -n $NS apply -f mvs-proxy.yaml
+
+  echo Installing mvs-ui
+  helm -n $NS install mvs-ui mosip-helm/mvs-ui --set image.repository=niraqa/manual-verification-system-ui --set image.tag=tf_nira_qa_new --set mvs.apiUrl=https://$mvs_HOST --set istio.hosts[0]=$mvs_HOST --version $CHART_VERSION
+
+  echo Installing ms service. Will wait till service gets installed.
+  helm -n $NS install mvs-service mosip-helm/mvs-service --set image.repository=niraqa/manual-verification-service -set image.tag=tf_nira_qa_new --version $CHART_VERSION --wait
+
+  kubectl -n $NS  get deploy -o name |  xargs -n1 -t  kubectl -n $NS rollout status
+
+  echo Installed mvs services
+
+  echo "manual-verification-system portal URL: https://$mvs_HOST/mvs-ui/"
+  return 0
+}
+
+# set commands for error handling.
+set -e
+set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
+set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
+set -o errtrace  # trace ERR through 'time command' and other functions
+set -o pipefail  # trace ERR through pipes
+installing_mvs   # calling function
